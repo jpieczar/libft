ff6055c88635377273541577c6779853d4a7557e
Jonathan PIECZARA <jpieczar@c4r16s2.wethinkcode.co.za>
2019-05-29 08:20:03 +0200

    29-05-2019 atoi fixed

--- Done/ft_atoi.c
+++ Done/ft_atoi.c
@@ -6,55 +6,33 @@
 /*   By: jpieczar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:12:42 by jpieczar          #+#    #+#             */
-/*   Updated: 2019/05/24 11:12:03 by jpieczar         ###   ########.fr       */
+/*   Updated: 2019/05/29 08:15:32 by jpieczar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isnum(const char *str)
-{
-	int	i;
-
-	i = 0;
-	if ((str[i] == '-') || (str[i] == '+'))
-		i++;
-	while (str[i])
-	{
-		if ((str[i] >= '0') && (str[i] <= '9'))
-			i++;
-		else
-			return (0);
-	}
-	return (1);
-}
-
 int	ft_atoi(const char *str)
 {
-	if (ft_isnum(&*str))
-	{
-		int	i;
-		int	res;
-		int	mul;
+	int	i;
+	int	res;
+	int	mul;
 
-		i = 0;
-		mul = 1;
-		while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
-		if ((str[i] == '-') || (str[i] == '+'))
-		{
-			if (str[i] == '-')
-				mul = (-1);
-			i++;
-		}
-		res = (str[i] - 48);
+	i = 0;
+	res = 0;
+	mul = 1;
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
 		i++;
-		while (str[i])
-		{
-			res = ((res * 10) + (str[i] - 48));
-			i++;	
-		}
-		return (res * mul);
+	if ((str[i] == '-') || (str[i] == '+'))
+	{
+		if (str[i] == '-')
+			mul = (-1);
+		i++;
 	}
-	else
-		return (0);
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		res = ((res * 10) + (str[i] - 48));
+		i++;	
+	}
+	return (res * mul);
 }
