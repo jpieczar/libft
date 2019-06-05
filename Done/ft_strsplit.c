fb13fcd2e8c7a5a530d0b81522f0fb89db7ec6f2
Jonathan PIECZARA <jpieczar@c4r16s2.wethinkcode.co.za>
2019-06-05 07:49:23 +0200

    05-06-2019 split done

--- Done/ft_strsplit.c
+++ Done/ft_strsplit.c
@@ -6,16 +6,13 @@
 /*   By: jpieczar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 11:46:43 by jpieczar          #+#    #+#             */
-/*   Updated: 2019/06/04 16:02:56 by jpieczar         ###   ########.fr       */
+/*   Updated: 2019/06/05 07:46:59 by jpieczar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"*/
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-/*
- * static int	ft_wordcount(char *s, char	c)
+static int	ft_wordcount(char *s, char	c)
 {
 	int	i;
 	int	res;
@@ -34,7 +31,7 @@
 		}
 	}
 	return (res);
-}*/
+}
 
 static int	ft_wordlen(char *s, const char c)
 {
@@ -57,34 +54,30 @@ static int	ft_wordlen(char *s, const char c)
 	return (0);
 }
 
-void	ft_strsplit(const char *s, char c)
+char	**ft_strsplit(const char *s, char c)
 {
-	//char	**str;
-	//int		i;
+	char	**str;
+	int		i;
 	int		j;
+	int		k;
 
 	j = 0;
-	//i = ft_wordcount((char*)s, c);
-	while (s[j])
+	k = 0;
+	i = ft_wordcount((char*)s, c);
+	if (!(str = (char**)malloc(sizeof(char*) * i)))
+		return (NULL);
+	while (s[j] && (k != i))
 	{
 		if (s[j] == c)
 			j++;
 		if (s[j] != c && s[j])
 		{
-			printf("%d\n", ft_wordlen((char*)&s[j], c));
+			if(!(str[k] = (char*)malloc(sizeof(char) * ft_wordlen((char*)&s[j], c))))
+				return (NULL);
 			while (s[j] != c && s[j])
 				j++;
 		}
+		k++;
 	}
-}
-
-int	main(int argc, char **argv)
-{
-	char	c;
-
-	c = '*';
-	if (argc == 2)
-		ft_strsplit((const char*)argv[1], c);
-	else
-		return (0);
+	return (str);
 }
